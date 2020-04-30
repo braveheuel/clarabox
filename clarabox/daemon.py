449f3bd53be9c5250c5aee7139c2db32cd98d863
Christoph Heuel <christoph@heuel-web.de>
2020-04-30 02:10:58 +0200

    Catch KeyboardInterrupt

--- clarabox/daemon.py
+++ clarabox/daemon.py
@@ -39,4 +39,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+                logger.info("Process interrupted")
