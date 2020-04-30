895b5638f324d230b1aed1b4d2c09a0671390ab4
Christoph Heuel <christoph@heuel-web.de>
2020-04-30 02:11:14 +0200

    GPIO Module, first draft

---
+++ clarabox/gpio.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Distributed under terms of the MIT license.
+
+"""
+GPIO module
+"""
+import asyncio
+import logging
+import gpiozero
