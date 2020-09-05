216b0c268c3e961723e9435691c409f241037edf
Christoph Heuel <christoph@heuel-web.de>
2020-09-05 02:44:11 +0200

    LED Daemon
    * Initialize LED-Daemon
    * Set LEDs to fixed Colors (Somehow random)

--- clarabox/led-daemon.py
+++ clarabox/led-daemon.py
@@ -8,22 +8,38 @@
 LED Daemon (should run with root permissions)
 """
 import configparser
+import asyncio
+from rpi_ws281x import PixelStrip, Color
 
 cfg = configparser.ConfigParser()
 cfg.read("/etc/clarabox.conf")
 
-def main():
+strip = None
+
+def init():
+    global strip
     LED_COUNT = cfg["WS8211"].getint("count", 6)
     LED_PIN =  cfg["WS8211"].getint("pin", 12)
     LED_FREQ_HZ = cfg["WS8211"].getint("frequence", 800000)
     LED_DMA = cfg["WS8211"].getint("dma", 10)
     LED_INVERT = cfg["WS8211"].getboolean("invert", False)
-    LED_BRIGHTMESS = cfg["WS8211"].getint("brightness", 255)
+    LED_BRIGHTNESS = cfg["WS8211"].getint("brightness", 50)
     LED_CHANNEL = cfg["WS8211"].getint("channel", 0)
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA,
                        LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
 
+
+def main():
+    init()
+
+    strip.setPixelColor(0, Color(255, 0, 0))
+    strip.setPixelColor(1, Color(255, 255, 0))
+    strip.setPixelColor(2, Color(255, 255, 255))
+    strip.setPixelColor(3, Color(0, 255, 0))
+    strip.setPixelColor(4, Color(0, 255, 255))
+    strip.setPixelColor(5, Color(255, 255, 255))
+    strip.show()
 
 
 
